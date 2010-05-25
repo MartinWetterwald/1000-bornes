d79d709a8107cb72fe485eef917ee5d1685ff40c
DJ Fox <djfox@zcorrecteurs.fr>
2010-05-25 14:09:02 +0200

    - Réorganisation complète de l'architecture des dossiers. - Création du schéma de la structure de données.

---
+++ compte_rendu/SD.mm
@@ -0,0 +1,299 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783544487" ID="ID_1172248172" MODIFIED="1274786596536" TEXT="partie">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptpartie"/>
+<node CREATED="1274783544487" ID="ID_1555986026" MODIFIED="1274786597000" POSITION="left" TEXT="partie">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tpartie"/>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783651402" ID="ID_887705667" MODIFIED="1274786391032" TEXT="deck">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptdeck"/>
+<attribute NAME="Alias" VALUE="Tptliste"/>
+<node CREATED="1274785702252" ID="ID_1674788681" MODIFIED="1274786371323" TEXT="deck">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tdeck"/>
+<attribute NAME="Alias" VALUE="Tliste"/>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783738154" ID="ID_1368725906" MODIFIED="1274786608212" TEXT="premier">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274786642522" ID="ID_1044294972" MODIFIED="1274786702342" TEXT="carte">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tcarte"/>
+<attribute NAME="Alias" VALUE="Tmaillon"/>
+<node CREATED="1274786716463" ID="ID_108637942" MODIFIED="1274786743068" TEXT="valeur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274786748521" ID="ID_994915028" MODIFIED="1274786810699" TEXT="suivant">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783746040" ID="ID_1175348045" MODIFIED="1274786391028" TEXT="dernier">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274786642522" ID="ID_1743814731" MODIFIED="1274786702342" TEXT="carte">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tcarte"/>
+<attribute NAME="Alias" VALUE="Tmaillon"/>
+<node CREATED="1274786716463" ID="ID_1383067373" MODIFIED="1274786743068" TEXT="valeur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274786748521" ID="ID_530342822" MODIFIED="1274786810699" TEXT="suivant">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274787141602" ID="ID_1377882894" MODIFIED="1274787144310" TEXT="NULL"/>
+</node>
+</node>
+</node>
+<node CREATED="1274783751832" ID="ID_1615167405" MODIFIED="1274786371323" TEXT="taille">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783667191" ID="ID_925003775" MODIFIED="1274786391031" TEXT="joueur_selectionne">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptjoueur"/>
+<node CREATED="1274787212893" ID="ID_853024914" MODIFIED="1274787291568" TEXT="joueur">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tjoueur"/>
+<node CREATED="1274787277758" ID="ID_656334706" MODIFIED="1274787277758" TEXT=""/>
+<node CREATED="1274787342502" ID="ID_17349629" MODIFIED="1274787357844" TEXT="est_ordinateur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787374107" ID="ID_348974738" MODIFIED="1274787386523" TEXT="difficulte_ordinateur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787389559" ID="ID_1007675522" MODIFIED="1274787414799" TEXT="nom">
+<attribute NAME="Type" VALUE="char*"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783651402" ID="ID_155780835" MODIFIED="1274786391032" TEXT="deck">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptdeck"/>
+<attribute NAME="Alias" VALUE="Tptliste"/>
+<node CREATED="1274785702252" ID="ID_787199157" MODIFIED="1274786371323" TEXT="deck">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tdeck"/>
+<attribute NAME="Alias" VALUE="Tliste"/>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783738154" ID="ID_1925812488" MODIFIED="1274786608212" TEXT="premier">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274786642522" ID="ID_163537517" MODIFIED="1274786702342" TEXT="carte">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tcarte"/>
+<attribute NAME="Alias" VALUE="Tmaillon"/>
+<node CREATED="1274786716463" ID="ID_291884920" MODIFIED="1274786743068" TEXT="valeur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274786748521" ID="ID_1431473380" MODIFIED="1274786810699" TEXT="suivant">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783746040" ID="ID_617939264" MODIFIED="1274786391028" TEXT="dernier">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274786642522" ID="ID_235221229" MODIFIED="1274786702342" TEXT="carte">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tcarte"/>
+<attribute NAME="Alias" VALUE="Tmaillon"/>
+<node CREATED="1274786716463" ID="ID_989241238" MODIFIED="1274786743068" TEXT="valeur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274786748521" ID="ID_576669637" MODIFIED="1274786810699" TEXT="suivant">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274787141602" ID="ID_1024015011" MODIFIED="1274787144310" TEXT="NULL"/>
+</node>
+</node>
+</node>
+<node CREATED="1274783751832" ID="ID_689692392" MODIFIED="1274786371323" TEXT="taille">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+</node>
+</node>
+<node CREATED="1274787529052" ID="ID_829798161" MODIFIED="1274787537169" TEXT="cumul_bornes">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787556199" ID="ID_1932504269" MODIFIED="1274787561497" TEXT="nb_bottes_jouees">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787580596" ID="ID_1568213549" MODIFIED="1274787589336" TEXT="nb_coups_fourres_joues">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787580596" ID="ID_464433045" MODIFIED="1274787616007" TEXT="nb_200bornes_jouees">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787580596" ID="ID_283124154" MODIFIED="1274787624951" TEXT="couronnement">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_109877781" MODIFIED="1274788019042" TEXT="est_creve">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_516864101" MODIFIED="1274788019042" TEXT="a_accident">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_1223562297" MODIFIED="1274788019041" TEXT="en_panne_dessence">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_1178790191" MODIFIED="1274788019041" TEXT="est_limite_par_vitesse">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_765108018" MODIFIED="1274788019041" TEXT="est_arrete">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_1415940994" MODIFIED="1274788089935" TEXT="increvable">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_1981768952" MODIFIED="1274788089935" TEXT="citerne">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_1849834251" MODIFIED="1274788089935" TEXT="as_du_volant">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_1654092765" MODIFIED="1274788089934" TEXT="prioritaire">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783679406" ID="ID_1220072356" MODIFIED="1274786391030" TEXT="autre_joueur">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptjoueur"/>
+<node CREATED="1274787212893" ID="ID_158487305" MODIFIED="1274787291568" TEXT="joueur">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tjoueur"/>
+<node CREATED="1274787277758" ID="ID_1282195365" MODIFIED="1274787277758" TEXT=""/>
+<node CREATED="1274787342502" ID="ID_950295343" MODIFIED="1274787357844" TEXT="est_ordinateur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787374107" ID="ID_884787077" MODIFIED="1274787386523" TEXT="difficulte_ordinateur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787389559" ID="ID_449155153" MODIFIED="1274787414799" TEXT="nom">
+<attribute NAME="Type" VALUE="char*"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783651402" ID="ID_638267890" MODIFIED="1274786391032" TEXT="deck">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptdeck"/>
+<attribute NAME="Alias" VALUE="Tptliste"/>
+<node CREATED="1274785702252" ID="ID_77350881" MODIFIED="1274786371323" TEXT="deck">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tdeck"/>
+<attribute NAME="Alias" VALUE="Tliste"/>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783738154" ID="ID_1645848021" MODIFIED="1274786608212" TEXT="premier">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274786642522" ID="ID_462872285" MODIFIED="1274786702342" TEXT="carte">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tcarte"/>
+<attribute NAME="Alias" VALUE="Tmaillon"/>
+<node CREATED="1274786716463" ID="ID_934920977" MODIFIED="1274786743068" TEXT="valeur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274786748521" ID="ID_1578957653" MODIFIED="1274786810699" TEXT="suivant">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274783746040" ID="ID_1618469128" MODIFIED="1274786391028" TEXT="dernier">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274786642522" ID="ID_500240470" MODIFIED="1274786702342" TEXT="carte">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="Tcarte"/>
+<attribute NAME="Alias" VALUE="Tmaillon"/>
+<node CREATED="1274786716463" ID="ID_433567056" MODIFIED="1274786743068" TEXT="valeur">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node BACKGROUND_COLOR="#c0d2e9" CREATED="1274786748521" ID="ID_277673931" MODIFIED="1274786810699" TEXT="suivant">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="up"/>
+<attribute NAME="Type" VALUE="Tptcarte"/>
+<attribute NAME="Alias" VALUE="Tptmaillon"/>
+<node CREATED="1274787141602" ID="ID_1372558621" MODIFIED="1274787144310" TEXT="NULL"/>
+</node>
+</node>
+</node>
+<node CREATED="1274783751832" ID="ID_1849885895" MODIFIED="1274786371323" TEXT="taille">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+</node>
+</node>
+<node CREATED="1274787529052" ID="ID_30843731" MODIFIED="1274787537169" TEXT="cumul_bornes">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787556199" ID="ID_241660563" MODIFIED="1274787561497" TEXT="nb_bottes_jouees">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787580596" ID="ID_1225767403" MODIFIED="1274787589336" TEXT="nb_coups_fourres_joues">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787580596" ID="ID_511913333" MODIFIED="1274787616007" TEXT="nb_200bornes_jouees">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node CREATED="1274787580596" ID="ID_1184743588" MODIFIED="1274787624951" TEXT="couronnement">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_487209060" MODIFIED="1274788019042" TEXT="est_creve">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_1546408201" MODIFIED="1274788019042" TEXT="a_accident">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_556154350" MODIFIED="1274788019041" TEXT="en_panne_dessence">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_1885812536" MODIFIED="1274788019041" TEXT="est_limite_par_vitesse">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff0000" CREATED="1274787580596" ID="ID_306581256" MODIFIED="1274788019041" TEXT="est_arrete">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_30457565" MODIFIED="1274788089935" TEXT="increvable">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_1606278257" MODIFIED="1274788089935" TEXT="citerne">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_1154829756" MODIFIED="1274788089935" TEXT="as_du_volant">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+<node COLOR="#ff7100" CREATED="1274787580596" ID="ID_86470099" MODIFIED="1274788089934" TEXT="prioritaire">
+<attribute NAME="Type" VALUE="int"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
